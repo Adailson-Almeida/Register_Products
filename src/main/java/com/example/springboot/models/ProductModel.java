8837372fb9b13d43adb22d596d217d5f0ccef479
Adailson-Almeida <adailson.almeida@outlook.com.br>
2024-06-13 12:59:58 -0300

    inclusão do atributo description

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -20,6 +20,7 @@ public class ProductModel extends RepresentationModel<ProductModel> implements S
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
     private String name;
+    private String description;
     private BigDecimal value;
 
     public UUID getIdProduct() {
@@ -44,5 +45,13 @@ public class ProductModel extends RepresentationModel<ProductModel> implements S
 
     public void setValue(BigDecimal value) {
         this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
